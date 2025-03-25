a0ea66153413593d1d85a0805a7253be9eac5e66
guswls8859 <guswls8859@naver.com>
2025-03-25 16:36:24 +0900

    db구축중

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Greeting(models.Model):
+    title = models.TextField(null=True, default='제목을 입력해주세요')
+    content = models.TextField(null=True, default='내용을 입력해주세요')
+    name = models.CharField(max_length=10, default='이름을 입력해주세요')
+
