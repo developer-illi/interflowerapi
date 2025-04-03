9bf68d1c4e9b6f14e6a10f18344a806f0dd59abe
guswls8859 <guswls8859@naver.com>
2025-04-03 17:01:38 +0900

    gg

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,14 @@ class Greeting_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Association_greeting
         fields = '__all__'
+
+
+class History_year_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Association_history_year
+        fields = '__all__'
+
+class History_month_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Association_history_month
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -10,3 +10,12 @@ from .serializers import *
 class Greeting_ViewSet(viewsets.ModelViewSet):
     queryset = Association_greeting.objects.all()
     serializer_class = Greeting_Serializer
+
+class history_year_Viewset(viewsets.ModelViewSet):
+    queryset = Association_history_year.objects.all()
+    serializer_class = History_year_Serializer
+
+class History_month_Viewset(viewsets.ModelViewSet):
+    queryset = Association_history_month.objects.all()
+    serializer_class = History_month_Serializer
+
